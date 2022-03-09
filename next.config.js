984f78b7146fa450e78497e24a5ffb8cfda90beb
Jarek Batorski <jarekb@gmail.com>
2022-03-08 21:54:59 -0600

    Fix basePath issue

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: isProd ? '/me/' : '',
-  basePath: isProd ? '/me/' : '',
+  basePath: isProd ? '/me' : '',
 }
 
 module.exports = nextConfig
