591d7de6c8387e4f558c5457504f0f2023865cef
Jarek Batorski <jarekb@gmail.com>
2022-03-20 15:24:07 -0500

    Handle displaying dates

--- components/resume/job/job.tsx
+++ components/resume/job/job.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import { Job } from '../types';
 import Position from './position';
+import DateRange from './dateRange/dateRange';
 
-const Job: FC<Job> = ({ companyName, positions, highlights }) => {
+const Job: FC<Job> = ({ companyName, positions, highlights, startDate, endDate }) => {
     return <div>
         <h3>{companyName}</h3>
+        <div><DateRange start={startDate} end={endDate} /></div>
         <div>
             {positions.map(position => <Position key={`${companyName} ${position.title}`} {...position} />)}
         </div>
