7566b06874266f0f24b96992017297c7edd34c48
Jarek Batorski <jarekb@gmail.com>
2022-04-20 17:52:48 -0500

    Testing linting on commit

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -4,7 +4,9 @@ import Navbar from './navbar/navbar';
 
 const Layout: FC = ({ children }) => {
     return <>
-        <Head>            <title>JBX</title>            <meta name="description" content="All about Jerry" />            <link rel="icon" href="/favicon.ico" />
+        <Head>            <title>JBX</title>
+            <meta name="description" content="All about Jerry" />
+            <link rel="icon" href="/favicon.ico" />
         </Head>
         <Navbar />
         <main>
