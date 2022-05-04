0daf1dc24c3feff0b2bb50b957ef3d215a52025e
Jerry Batorski <jarekb@gmail.com>
2022-05-03 23:02:25 -0500

    29 Update home page and styling (#30)
    * 29 Update home page and styling
    
    * Add note bout responseive designs

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,39 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
     return (
-        <div>
-            <h1>Home!</h1>
+        <div className="home">
+            <h1>Home</h1>
+            <p className="home__intro">
+                Welcome to my resume GitHub pages site.
+                This is a SPA built with React, TypeScript, and SASS on top of Next.JS.
+            </p>
+            <div className="home__content">
+                <p>There were multiple motivations behind building this site:</p>
+                <ul >
+                    <li>Demonstrate how I tackle projects and write applications</li>
+                    <ul>
+                        <li>
+                        Checkout the GitHub
+                            {' '}
+                            <a href="https://github.com/jarekb84/my=" target="_blank" rel="noreferrer">repository</a>
+                            {' '}
+                        for this site
+                        </li>
+                    </ul>
+                    <li>Store my resume in Git to retain historical changes</li>
+                    <li>More consistent styling for the resume layout versus lots of manual tweaks in Google Docs</li>
+                    <li>
+                    Utilize the
+                        {' '}
+                        <a href="https://jsonresume.org" target="_blank" rel="noreferrer">JSON Resume</a>
+                        {' '}
+                    to structure my resume data
+                    </li>
+                    <li>Make sure the resume can easily be viewed on any device and be printed</li>
+                </ul>
+            </div>
+
+
         </div>
     );
 };
