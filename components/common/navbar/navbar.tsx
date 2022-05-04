006390c2a42cc7a9085b56c9687a6de0fd389248
Jarek Batorski <jarekb@gmail.com>
2022-05-03 22:49:22 -0500

    29 Update home page and styling

--- components/common/navbar/navbar.tsx
+++ components/common/navbar/navbar.tsx
@@ -1,15 +1,38 @@
 
 import React, { FC } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const links = [
+    {
+        href: '/',
+        title: 'Home',
+    },
+    {
+        href: 'https://github.com/jarekb84/my',
+        title: 'GitHub',
+        target: '_blank',
+        rel: 'noreferrer',
+    },
+    {
+        href: '/resume',
+        title: 'Resume',
+    },
+];
 
 const Navbar: FC = () => {
-    return <nav className="navbar">
-        <Link href="/">
-            <a>Home</a>
-        </Link>
-        <Link href="/resume">
-            <a>Resume</a>
-        </Link>
+    const { asPath } = useRouter();
+
+    return <nav className="navbar status">
+        {links.map(({ href, title, target, rel }) => {
+            return <Link href={href} key={href}>
+                <a className={`navbar__link ${asPath === href ? 'navbar__link--active' : ''}`}
+                    target={target}
+                    rel={rel}>
+                    {title}
+                </a>
+            </Link>;
+        })}
     </nav>;
 };
 
