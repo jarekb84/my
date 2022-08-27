685235e49478cb817846aa8419295371ad1aa769
Jarek Batorski <jarekb@gmail.com>
2022-08-27 15:56:32 -0500

    34 Fix dynamic page titles

--- components/resume/resume.tsx
+++ components/resume/resume.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import Layout from '../common/layout';
 import Job from './job/job';
 import Skills from './skills/skills';
 import { Basics, Job as IJob } from './types';
@@ -10,34 +11,36 @@ interface ResumeProps {
 }
 
 const Resume: FC<ResumeProps> = ({ basics, skills, jobs }) => {
-    return <div className="resume">
-        <div className="resume__contact-info">
-            <h1>{basics.name}</h1>
-            <div className="resume__contact-info__basics">
-                <div>
-                    <strong>Email </strong>
-                    <a href={`mailto:${basics.email}`}>
-                        {basics.email}
-                    </a>
-                </div>
-                <div>
-                    <strong>Github </strong>
-                    <a href={basics.url} target="_blank" rel="noreferrer">
-                        {basics.url.replace('https://', '')}
-                    </a>
+    return <Layout pageTitle="Resume">
+        <div className="resume">
+            <div className="resume__contact-info">
+                <h1>{basics.name}</h1>
+                <div className="resume__contact-info__basics">
+                    <div>
+                        <strong>Email </strong>
+                        <a href={`mailto:${basics.email}`}>
+                            {basics.email}
+                        </a>
+                    </div>
+                    <div>
+                        <strong>Github </strong>
+                        <a href={basics.url} target="_blank" rel="noreferrer">
+                            {basics.url.replace('https://', '')}
+                        </a>
+                    </div>
                 </div>
             </div>
+            <div>
+                {basics.summary}
+            </div>
+            <h2>Skills</h2>
+            <div>
+                <Skills skills={skills} />
+            </div>
+            <h2>Professional Experience</h2>
+            {jobs.map(job => <Job {...job} key={job.companyName} />)}
         </div>
-        <div>
-            {basics.summary}
-        </div>
-        <h2>Skills</h2>
-        <div>
-            <Skills skills={skills} />
-        </div>
-        <h2>Professional Experience</h2>
-        {jobs.map(job => <Job {...job} key={job.companyName} />)}
-    </div>;
+    </Layout>;
 };
 
 export default Resume;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Layout from '../components/common/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Layout pageName={Component.name}>
-        <Component {...pageProps} />
-    </Layout>;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
