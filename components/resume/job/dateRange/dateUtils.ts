48907b018b54584786c52fce34fc4be0d93558eb
Jarek Batorski <jarekb@gmail.com>
2025-05-07 23:07:10 -0500

    Update testing related packages

--- components/resume/job/dateRange/dateUtils.ts
+++ components/resume/job/dateRange/dateUtils.ts
@@ -1,4 +1,4 @@
-import { parseISO, format } from 'date-fns';
+import { parseISO, format, isValid } from 'date-fns';
 
 export function formatDate(dateVal: string | null, formatPattern: string, fallback = '') {
     if (!dateVal) {
@@ -8,9 +8,13 @@ export function formatDate(dateVal: string | null, formatPattern: string, fallba
     try {
         const parsedDate = parseISO(dateVal);
 
+        if (!isValid(parsedDate)) {
+            return fallback;
+        }
+
         return format(parsedDate, formatPattern);
     } catch (error) {
-        console.error(error);
+        console.error('Error formatting date:', error);
 
         return fallback;
     }
